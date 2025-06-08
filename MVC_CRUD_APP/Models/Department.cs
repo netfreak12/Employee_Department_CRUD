930ac4ac3ad1a49fc09239c2cabbb555323f2cfb
netfreak12 <lokhandesandesh1998@gmail.com>
2025-06-08 19:18:20 +0530

    Department Model class created

---
+++ MVC_CRUD_APP/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MVC_CRUD_APP.Models
+{
+    public class Department
+    {
+        [Key] // Assuming you are using System.ComponentModel.DataAnnotations for the Key attribute
+        public int DeptId { get; set; }
+
+        [Required(ErrorMessage = "Department Name is required.")]
+        public string DeptName { get; set; } = string.Empty; // Default to an empty string to avoid null reference issues
+    }
+}
