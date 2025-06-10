573a51c066ac4a5c0186592baf3c88988fd271de
netfreak12 <lokhandesandesh1998@gmail.com>
2025-06-10 12:36:50 +0530

    Done with Intial Migration and updated the database

---
+++ MVC_CRUD_APP/Migrations/20250610070537_Intial Migration.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_CRUD_APP.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DeptId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeptName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DeptId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpFirstName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    EmpLastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EmailId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EmpGender = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    EmpPhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false),
+                    EmpStatus = table.Column<bool>(type: "bit", nullable: false),
+                    DeptId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmpId);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_DeptId",
+                        column: x => x.DeptId,
+                        principalTable: "Departments",
+                        principalColumn: "DeptId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DeptId",
+                table: "Employees",
+                column: "DeptId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
