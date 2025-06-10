53dfc89677c0348ec83c07b8839cd1cb13c134ef
netfreak12 <lokhandesandesh1998@gmail.com>
2025-06-10 12:50:21 +0530

    Updated some code

--- MVC_CRUD_APP/Program.cs
+++ MVC_CRUD_APP/Program.cs
@@ -7,13 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MVC_CRUD_APP_DbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MVC_CRUD_APP_ConnectionString")));
 
+// Register MVC services
+builder.Services.AddControllersWithViews();
+
+// Add authorization services
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts. 
     app.UseHsts();
 }
 
